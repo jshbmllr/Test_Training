ef5a4be21aff3ccce4a107bb0296bb916c3b4a18
jshbmllr <miller.joshuab@gmail.com>
2017-05-18 22:55:29 -0600

    Added the aRandomNumber and aReturnlessFunction tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,8 @@
 var timesTwo = require("../code.js").timesTwo;
 var dividedByTen = require("../code.js").dividedByTen;
 var reverseArray = require("../code.js").reverseArray;
+var aRandomNumber = require("../code.js").aRandomNumber;
+var aReturnlessFunction = require("../code.js").aReturnlessFunction;
 
 test("The timesTwo function", () => {
   expect(timesTwo(2)).toBe(4);
@@ -12,4 +14,15 @@ test("The dividedByTen function", () => {
 
 test("The reverseArray function", () => {
   expect(reverseArray(["f", "r", "a", "n", "k"])).toEqual(["k", "n", "a", "r", "f"]);
+});
+
+test("The aRandomNumber function", () => {
+  expect(aRandomNumber()).toBeLessThan(11);
+  expect(aRandomNumber()).toBeGreaterThanOrEqual(1);
+  expect(typeof aRandomNumber()).toBe("number");
+});
+
+test("The aReturnlessFunction function", () => {
+  expect(aReturnlessFunction()).toBeFalsy();
+  expect(aReturnlessFunction()).toHaveBeenCalled();
 });
